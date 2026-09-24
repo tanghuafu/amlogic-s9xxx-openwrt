9a56b4232b6ec77b2d0edfdbc4a21d570982b04f
tanghuafu <161001306+tanghuafu@users.noreply.github.com>
2026-09-24 10:09:37 +0800

    Update diy-part2.sh

--- config/immortalwrt_master/diy-part2.sh
+++ config/immortalwrt_master/diy-part2.sh
@@ -49,55 +49,7 @@ fi
 rm -rf package/luci-app-amlogic
 git clone -b main https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 #
-# ===================== OpenVPN 2.6.12 =====================
-echo "Remove feeds openvpn..."
-rm -rf feeds/packages/net/openvpn
-rm -rf package/openvpn
 
-echo "Clone OpenVPN v2.6.12 source..."
-git clone --depth=1 -b v2.6.12 https://mirror.ghproxy.com/https://github.com/OpenVPN/openvpn.git package/openvpn
-if [ $? -ne 0 ];then
-    echo "ERROR: OpenVPN source clone failed"
-    exit 2
-fi
-
-echo "Clone openwrt-23.05 packages for Makefile..."
-rm -rf /tmp/openvpn-feed-tmp
-# 只拉指定目录，不要全仓packages，体积巨大、极易超时
-git clone --depth=1 -b openwrt-23.05 --sparse https://mirror.ghproxy.com/https://github.com/openwrt/packages.git /tmp/openvpn-feed-tmp
-cd /tmp/openvpn-feed-tmp
-git sparse-checkout set net/openvpn
-cd $GITHUB_WORKSPACE
-if [ -d "/tmp/openvpn-feed-tmp/net/openvpn" ];then
-    cp -r /tmp/openvpn-feed-tmp/net/openvpn/* package/openvpn/
-else
-    echo "ERROR: sparse checkout packages openvpn failed"
-    exit 2
-fi
-rm -rf /tmp/openvpn-feed-tmp
-
-# ===================== luci-app-openvpn 经典版 =====================
-echo "Remove luci-app-openvpn-server..."
-rm -rf feeds/luci/applications/luci-app-openvpn-server
-rm -rf package/luci-app-openvpn-server
-rm -rf package/luci-app-openvpn
-
-echo "Clone old luci 23.05 for luci-app-openvpn..."
-rm -rf /tmp/luci-old
-# sparse 稀疏检出，只拿 luci-app-openvpn，大幅减小下载量
-git clone --depth=1 -b openwrt-23.05 --sparse https://mirror.ghproxy.com/https://github.com/openwrt/luci.git /tmp/luci-old
-cd /tmp/luci-old
-git sparse-checkout set applications/luci-app-openvpn
-cd $GITHUB_WORKSPACE
-if [ -d "/tmp/luci-old/applications/luci-app-openvpn" ];then
-    cp -r /tmp/luci-old/applications/luci-app-openvpn package/luci-app-openvpn
-else
-    echo "ERROR: sparse checkout luci-app-openvpn failed"
-    exit 2
-fi
-rm -rf /tmp/luci-old
-
-echo "===== custom.sh finished ====="
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Additional customizations ends -------------------------------
